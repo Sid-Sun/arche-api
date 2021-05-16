3d56682e58385f0c602b1eff9f82fb8dbc593777
Sidharth Soni (Sid Sun) <sid@sidsun.com>
2021-05-16 13:25:31 +0530

    Implement Get folder in DB
    Implement a new method to get contents of a folder from DB at the
    database layer - introduce types which define contents for a type
    
    Signed-off-by: Sidharth Soni (Sid Sun) <sid@sidsun.com>

--- app/database/folders.go
+++ app/database/folders.go
@@ -11,6 +11,7 @@ import (
 )
 
 type FoldersTable interface {
+	Get(folderID types.FolderID, userID types.UserID) ([]types.FolderContent, *erx.Erx)
 	GetAll(userID types.UserID) ([]types.Folder, *erx.Erx)
 	Delete(folderID types.FolderID, UserID types.UserID) *erx.Erx
 	Create(name string, userID types.UserID) (types.FolderID, *erx.Erx)
@@ -21,10 +22,10 @@ type folders struct {
 	db  *sql.DB
 }
 
-func (f *folders) GetAll(userID types.UserID) ([]types.Folder, *erx.Erx) {
-	query := `SELECT folder_id, name FROM folders WHERE user_id=@user_id`
+func (f *folders) Get(folderID types.FolderID, userID types.UserID) ([]types.FolderContent, *erx.Erx) {
+	query := `SELECT note_id, name FROM notes WHERE folder_id=(SELECT folder_id FROM folders WHERE user_id=@user_id AND folder_id=@folder_id)`
 
-	rows, err := f.db.Query(query, sql.Named("user_id", userID))
+	rows, err := f.db.Query(query, sql.Named("user_id", userID), sql.Named("folder_id", folderID))
 	if err != nil {
 		sqlErr, errx := checkForSQLError(err)
 		if sqlErr != nil {
@@ -42,6 +43,65 @@ func (f *folders) GetAll(userID types.UserID) ([]types.Folder, *erx.Erx) {
 			f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [Get] [Close] %s", err.Error()))
 		}
 	}(rows)
+	folderContents := *new([]types.FolderContent)
+
+	for rows.Next() {
+		var noteID types.NoteID
+		var name string
+
+		err = rows.Scan(&noteID, &name)
+		if err != nil {
+			sqlErr, errx := checkForSQLError(err)
+			if sqlErr != nil {
+				errx = erx.WithArgs(errx, erx.SeverityError)
+				f.lgr.Error(fmt.Sprintf("[Database] [Folders] [Get] [Scan] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
+				return nil, errx
+			}
+			f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [Get] [Scan] %s", err.Error()))
+			return nil, errx
+		}
+
+		folderContents = append(folderContents, types.FolderContent{
+			NoteID: noteID,
+			Name: name,
+		})
+	}
+
+	if err := rows.Err(); err != nil {
+		sqlErr, errx := checkForSQLError(err)
+		if sqlErr != nil {
+			errx = erx.WithArgs(errx, erx.SeverityError)
+			f.lgr.Error(fmt.Sprintf("[Database] [Folders] [Get] [Err] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
+			return nil, errx
+		}
+		f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [Get] [Err] %s", err.Error()))
+		return nil, errx
+	}
+
+	return folderContents, nil
+}
+
+func (f *folders) GetAll(userID types.UserID) ([]types.Folder, *erx.Erx) {
+	query := `SELECT folder_id, name FROM folders WHERE user_id=@user_id`
+
+	rows, err := f.db.Query(query, sql.Named("user_id", userID))
+	if err != nil {
+		sqlErr, errx := checkForSQLError(err)
+		if sqlErr != nil {
+			errx = erx.WithArgs(errx, erx.SeverityError)
+			f.lgr.Error(fmt.Sprintf("[Database] [Folders] [GetAll] [Query] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
+			return nil, errx
+		}
+		f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [GetAll] [Query] %d : %s", sqlErr.Number, sqlErr.Error()))
+		return nil, errx
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [GetAll] [Close] %s", err.Error()))
+		}
+	}(rows)
 	folders := *new([]types.Folder)
 
 	for rows.Next() {
@@ -53,10 +113,10 @@ func (f *folders) GetAll(userID types.UserID) ([]types.Folder, *erx.Erx) {
 			sqlErr, errx := checkForSQLError(err)
 			if sqlErr != nil {
 				errx = erx.WithArgs(errx, erx.SeverityError)
-				f.lgr.Error(fmt.Sprintf("[Database] [Folders] [Get] [Scan] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
+				f.lgr.Error(fmt.Sprintf("[Database] [Folders] [GetAll] [Scan] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
 				return nil, errx
 			}
-			f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [Get] [Scan] %s", err.Error()))
+			f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [GetAll] [Scan] %s", err.Error()))
 			return nil, errx
 		}
 
@@ -71,10 +131,10 @@ func (f *folders) GetAll(userID types.UserID) ([]types.Folder, *erx.Erx) {
 		sqlErr, errx := checkForSQLError(err)
 		if sqlErr != nil {
 			errx = erx.WithArgs(errx, erx.SeverityError)
-			f.lgr.Error(fmt.Sprintf("[Database] [Folders] [Get] [Err] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
+			f.lgr.Error(fmt.Sprintf("[Database] [Folders] [GetAll] [Err] [sqlErr] %d : %s", sqlErr.Number, sqlErr.Error()))
 			return nil, errx
 		}
-		f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [Get] [Err] %s", err.Error()))
+		f.lgr.Debug(fmt.Sprintf("[Database] [Folders] [GetAll] [Err] %s", err.Error()))
 		return nil, errx
 	}
 
---
+++ app/types/db_content_types.go
@@ -0,0 +1,6 @@
+package types
+
+type FolderContent struct {
+	NoteID NoteID
+	Name   string
+}
