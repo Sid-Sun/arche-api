31dab652a8616709768ba6a8927761f7be92d802
Sidharth Soni (Sid Sun) <sid@sidsun.com>
2021-05-16 15:55:04 +0530

    Introduce ContextURLParams middleware
    Introduce a new middleware which accepts a list of url params, fetches
    them from request, creates a map thereof and adds it to request context
    for further use
    
    Signed-off-by: Sidharth Soni (Sid Sun) <sid@sidsun.com>

--- app/middlewares/middlewares.go
+++ app/middlewares/middlewares.go
@@ -1,7 +1,11 @@
 package middlewares
 
 import (
+	"context"
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"go.uber.org/zap"
 )
 
 func WithContentJSON(next http.Handler) http.Handler {
@@ -9,4 +13,17 @@ func WithContentJSON(next http.Handler) http.Handler {
 		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, req)
 	})
+}
+
+func ContextURLParams(lgr *zap.Logger, params ...string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			m := make(map[string]string)
+			for _, param := range params {
+				m[param] = chi.URLParam(req, param)
+			}
+			ctx := context.WithValue(req.Context(), "url_params", m)
+			next.ServeHTTP(w, req.WithContext(ctx))
+		})
+	}
 }
